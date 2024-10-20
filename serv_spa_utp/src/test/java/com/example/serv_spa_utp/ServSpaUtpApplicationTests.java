f949e0a77c084fe4becd45e19e1a2470070ee2ab
JoseUG04 <u20311125@utp.edu.pe>
2024-10-19 19:24:11 -0500

    GET READY : Time for VERSION 2.0 : We change our project into a SpringBoot Framework!

---
+++ serv_spa_utp/src/test/java/com/example/serv_spa_utp/ServSpaUtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serv_spa_utp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServSpaUtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
